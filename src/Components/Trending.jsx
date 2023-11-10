c56596c4a212a33ac46ab8f59c4c34775953a088
Samuel Njoroge <96120597+Sam516-Dev@users.noreply.github.com>
2023-11-10 23:59:27 +0300

    test

--- src/Components/Trending.jsx
+++ src/Components/Trending.jsx
@@ -1,57 +1,50 @@
 import { useEffect, useState } from "react";
-
+import { AiOutlineShoppingCart } from "react-icons/ai";
 const Trending = () => {
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
+
+  // const [loading, setLoading] = useState(false);
 
   async function fetchData() {
     const response = await fetch("https://fakestoreapi.com/products");
     const fetchedData = await response.json();
     setData(fetchedData);
-    setLoading(false);
+    // setLoading(false);
     console.log("data from useEffect hook ", fetchedData);
   }
 
-  // useEffect(() => {
-  //   fetchData();
-  //   console.log("fetching data... ");
-  // }, []);
-
-  const handleClik = () => {
-    setTimeout(() => {
-      fetchData();
-    }, 3000 );
-    setLoading(true);
-  };
+  useEffect(() => {
+    fetchData();
+    console.log("fetching data... ");
+  }, []);
 
   return (
     <div className="mt-20 h-screen">
       <h1 className="text-2xl font-black text-blue-900 mt-7">
         Trending Products
       </h1>
-      <button
-        className="p-6 px-10 text-white bg-orange-500"
-        onClick={handleClik}
-      >
-        Click me
-      </button>
 
-      {loading ? (
-        <p className="p-3 text-orange-500 mt-5 border border-1 border-orange-500 bg-orange-100 text-lg font-semibold">
-          {" "}
-          Loading data ....{" "}
-        </p>
-      ) : (
-        <div>
-          {data.map((item) => {
-            return (
-              <>
-                <p key={item.id}> {item.category}</p>
-              </>
-            );
-          })}
-        </div>
-      )}
+      <div>
+        {data.map((item) => (
+
+
+          <div key={item.id} className=" w-52 h-96 border-2 border-blue-500">
+
+            <img src={item.image} alt="" className=" w-48 h-48 object-cover" />
+            <label> {item.category}</label>
+            <h3> {item.title}</h3>
+            <p> {item.rating.rate}</p>
+            <p>{item.price}</p>
+            <div>
+              <AiOutlineShoppingCart />
+              <button> Add To Cart </button>
+            </div>
+
+          </div>
+
+
+        ))}
+      </div>
     </div>
   );
 };
