7028d7fa867dd1fa5d3e987945f4fad8fd13a6a4
Samuel Njoroge <96120597+Sam516-Dev@users.noreply.github.com>
2023-12-08 00:06:31 +0300

    small changes

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -7,10 +7,10 @@ const Cart = () => {
     <div className=" w-1/3 h-1/3 bg-green-100 absolute p-3">
      <div className="flex justify-between items-center">
         <div className="flex gap-2 items-center">
-          <h1 className=" font-semibold text-2xl">SHOPPING CART</h1>
-          <label className="bg-orange-500 rounded-full  px-4 p-2 font-semibold text-xl">5</label>
+          <h1 className=" font-semibold text-xl">SHOPPING CART</h1>
+          <label className="bg-orange-500 rounded-full px-5  font-semibold text-xl">5</label>
          </div>
-         <RxCross2 className=" w-9 h-9" />
+         <RxCross2 className=" w-7 h-7" />
       </div>
       <div> </div>
       <div> </div>
