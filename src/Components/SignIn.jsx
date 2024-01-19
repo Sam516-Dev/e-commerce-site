18349c3daf60ebd521aa086d6b6b9e52f52fe9d8
Samuel Njoroge <96120597+Sam516-Dev@users.noreply.github.com>
2024-01-20 00:34:33 +0300

    we added alot, state, contextAPI and cart Component

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -1,75 +1,11 @@
-import React from "react";
-// import "./Navbar.css";
-function Navbar() {
+import React from 'react'
+
+const SignIn = () => {
   return (
-      <div className="navbar-container">
+    <div className='h-screen bg-blue-500'>
+      <p className='pt-28'> Hello from Our Signing ! </p>
+    </div>
+  )
+}
 
-        <ul className="navbarleft">
-
-          <li className=" dropdown">
-            <a className="mylink" href="#">
-              Men
-            </a>
-            <ul>
-              <li>
-                <a href="#">Shirts</a>
-              </li>
-              <li>
-                <a href="#">Trousers</a>
-              </li>
-              <li>
-                <a href="#">shoes</a>
-              </li>
-            </ul>
-          </li>
-          
-          <li>
-            <a className="mylink" href="#">
-              Women
-            </a>
-            <ul>
-              <li>
-                <a href="#">Dresses</a>
-              </li>
-              <li>
-                <a href="#">skirts</a>
-              </li>
-              <li>
-                <a href="#">Blouses</a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a className="mylink" href="#">
-              Gifts
-            </a>
-            <ul>
-              <li>
-                <a href="#">jewelry</a>
-              </li>
-              <li>
-                <a href="#">Bags</a>
-              </li>
-              <li>
-                <a href="#">watches</a>
-              </li>
-            </ul>
-          </li>
-          </ul>
-
-        <ul className="navbarright">
-          <li>
-            <a href="#">SEARCH</a>
-          </li>
-          <li>
-            <a href="#">SIGN IN</a>
-          </li>
-          <li>
-            <a href="#">BAG</a>
-          </li>
-        </ul>
-
-      </div>
-  );
- }
- export default Navbar;
+export default SignIn
